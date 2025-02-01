e35942b67b29dc7664f4f7fa5ddaaf0294fa83c2
Vinh Le <vinhpq.dt.468@gmail.com>
2025-02-01 15:33:15 +0700

    commit ne

---
+++ src/main/java/vn/tayjava/identity_service/dto/request/RoleRequest.java
@@ -0,0 +1,18 @@
+package vn.tayjava.identity_service.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RoleRequest {
+    String name;
+    String description;
+    Set<String> permissions;
+}
---
+++ src/main/java/vn/tayjava/identity_service/dto/respone/RoleResponse.java
@@ -0,0 +1,18 @@
+package vn.tayjava.identity_service.dto.respone;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RoleResponse {
+    String name;
+    String description;
+    Set<PermissionResponse> permissions;
+
+}
